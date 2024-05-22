d8cfeea84ab1303a0d8fb4d2263c4dc8a645c6dd
Juliette Tripier <juli.tripier@gmail.com>
2024-05-22 14:29:46 -0400

    Set up basic websocket route

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 import uvicorn
@@ -15,43 +15,19 @@ def read_index():
         html_content = index_html.read()
     return HTMLResponse(content = html_content)
 
+# create a new websocket route at /ws
+@app.websocket('/ws')
+async def websocket_endpoint(websocket: WebSocket):
+    # server accepts ws connection when client connects to server
+    await websocket.accept()
+    while True:
+        # server waits for client to send message
+        message = await websocket.receive_text()
+        # server sends response back to client
+        await websocket.send_text(f'Message: {message}')
+
+
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
-
-
-# from fastapi import FastAPI, HTTPException
-# from fastapi.responses import HTMLResponse
-# from fastapi.staticfiles import StaticFiles
-# from starlette.middleware import Middleware
-# from starlette.middleware.base import BaseHTTPMiddleware
-# import uvicorn
-# import mimetypes
-
-# class MIMETypeMiddleware(BaseHTTPMiddleware):
-#     async def dispatch(self, request, call_next):
-#         response = await call_next(request)
-#         if request.url.path.endswith('.js'):
-#             response.headers['Content-Type'] = 'application/javascript'
-#         return response
-
-# mimetypes.add_type("application/javascript", ".js", True)
-
-# middleware = [
-#     Middleware(MIMETypeMiddleware),
-# ]
-
-# app = FastAPI(middleware=middleware)
-
-# app.mount('/public', StaticFiles(directory='public', html=True), name='public')
-# app.mount('/src', StaticFiles(directory='src'), name='src')
-
-# @app.get("/", response_class=HTMLResponse)
-# def read_index():
-#     with open('public/index.html', 'r') as index_html:
-#         html_content = index_html.read()
-#     return HTMLResponse(content=html_content)
-
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
