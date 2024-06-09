7197c1fac46f05221439858d6acb5255612985df
Juliette Tripier <juli.tripier@gmail.com>
2024-06-09 10:19:02 -0400

    Server sends incrementing score to client

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, WebSocket
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse, RedirectResponse, FileResponse
 import uvicorn
+import asyncio
 
 app = FastAPI()
 
@@ -9,9 +10,13 @@ app.mount('/public', StaticFiles(directory = 'public', html = True), name = 'pub
 app.mount('/src', StaticFiles(directory = 'src'), name = 'src')
 app.mount('/build', StaticFiles(directory = 'build'), name = 'build')
 
+# @app.get('/static/js/main.250728df.js')
+# async def redirect_main_js():
+#     return RedirectResponse('http://localhost:8080/build/static/js/main.250728df.js')
+
 @app.get('/static/{rest_of_path:path}')
-async def redirect_to_build(request, rest_of_path):
-    response = RedirectResponse(url = f'/build/static/{rest_of_path}')
+async def redirect_to_build(rest_of_path):
+    response = RedirectResponse(f'http://localhost:8080/build/static/{rest_of_path}')
     return response
 
 @app.get("/", response_class = HTMLResponse)
@@ -20,14 +25,14 @@ def read_index():
         html_content = index_html.read()
     return HTMLResponse(content = html_content)
 
-@app.get('/{rest_of_path:path}')
-async def redirect_from_index(rest_of_path):
-    response = None
-    if '.png' in rest_of_path or '.ico' in rest_of_path:
-        response = FileResponse(f'build/{rest_of_path}')
-    if response is None:
-        print('Response was none')
-    return response
+# @app.get('/{rest_of_path:path}')
+# async def redirect_from_index(rest_of_path):
+#     response = None
+#     if '.png' in rest_of_path or '.ico' in rest_of_path:
+#         response = FileResponse(f'build/{rest_of_path}')
+#     if response is None:
+#         print('Response was none')
+#     return response
 
 
 # create a new websocket route at /ws
@@ -35,12 +40,24 @@ async def redirect_from_index(rest_of_path):
 async def websocket_endpoint(websocket: WebSocket):
     # server accepts ws connection when client connects to server
     await websocket.accept()
-    while True:
-        # server waits for client to send message
-        message = await websocket.receive_text()
-        # server sends response back to client
-        await websocket.send_text(f'Message: {message}')
+    score = Score()
+    score_gen = score.passive_generator()
+    async for score in score_gen:
+        await websocket.send_text(f'{score}')
 
+
+class Score():
+    def __init__(self):
+        self.score = 0
+    
+    async def passive_generator(self):
+        # use asyncio so you don't hold up the server
+        while True:
+            self.score += 1
+            yield self.score
+            await asyncio.sleep(1)
+
+            
 
 
 
--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ import store from './app/store'
 import { Provider } from 'react-redux'
 import reportWebVitals from './reportWebVitals';
 
+const socket = new WebSocket('/ws')
+socket.addEventListener('open', (event) => {
+  socket.send('Hi server');
+});
+socket.addEventListener('message', (event) => {
+  console.log(event.data);
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
