7554b5d9daab9b1b1bc6ad76741de21e76f03b72
Juliette Tripier <juli.tripier@gmail.com>
2024-06-10 14:23:45 -0400

    Clickables: Image now appears in a random spot on page reload. Next step: create these images and delete them on a time interval

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,13 @@
 import './App.css';
 import { Counter } from '../features/counter/counter'
+import { Clickable } from '../features/clickables/clickable'
+
+// use react to generate images in random locations at
+// random time intervals
+// add onclick functions to these images that send messages
+// to the server to increase the count
+// images should disappear/get deleted after being clicked
+// step one: generate an image at a random location
 
 function App() {
   return (
@@ -7,6 +15,7 @@ function App() {
       <header className="App-header">
         <h1>Hiiiiii</h1>
         <Counter />
+        <Clickable />
       </header>
     </div>
   );
